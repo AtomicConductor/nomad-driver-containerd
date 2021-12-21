f6e509ecf8814defaa63a5a38dcd8dea565ce3f9
Jonathan Cross <>
2021-12-21 14:25:19 -0500

    annotations test file
    wait for nomad service to be active after allow-privileged trap cleanup
    
    wait after submission
    
    debug status
    
    fix conditional checks

--- tests/011-test-annotations.sh
+++ tests/011-test-annotations.sh
@@ -2,23 +2,24 @@
 
 source $SRCDIR/utils.sh
 
+job_name=annotations
+
 test_annotations_nomad_job() {
     pushd ~/go/src/github.com/Roblox/nomad-driver-containerd/example
 
     echo "INFO: Starting nomad annotations job using nomad-driver-containerd."
     nomad job run -detach annotations.nomad
 
+    # Even though $(nomad job status) reports job status as "running"
+    # The actual container process might not be running yet.
+    echo "INFO: Wait for ${job_name} container to get into RUNNING state"
+    is_container_active ${job_name} true
+
     annotations_status=$(nomad job status -short annotations|grep Status|awk '{split($0,a,"="); print a[2]}'|tr -d ' ')
-    if [ annotations_status != "running" ];then
-        echo "ERROR: Error in getting annotations job status."
+    if [ "$annotations_status" != "running" ];then
+        echo "ERROR: Error in getting annotations job status. Has status of '$annotations_status'"
         exit 1
     fi
-
-    # Even though $(nomad job status) reports annotations job status as "running"
-    # The actual container process might not be running yet.
-    # We need to wait for actual container to start running before trying exec.
-    echo "INFO: Wait for annotations container to get into RUNNING state, before trying exec."
-    is_container_active annotations false
 
     echo "INFO: Check annotations are found when inspecting container"
     export CONTAINERD_NAMESPACE=nomad; ctr containers ls| grep annotations | cut -d ' ' -f1 | xargs ctr containers info | jq '.Spec.annotations.test' | tr -d '"' | xargs -I % test % = "annotations"
@@ -30,8 +31,8 @@ test_annotations_nomad_job() {
     echo "INFO: Stopping nomad annotations job."
     nomad job stop -detach annotations
     annotations_status=$(nomad job status -short annotations|grep Status|awk '{split($0,a,"="); print a[2]}'|tr -d ' ')
-    if [ $annotations_status != "dead(stopped)" ];then
-        echo "ERROR: Error in stopping annotations job."
+    if [ "$annotations_status" != "dead(stopped)" ];then
+        echo "ERROR: Error in stopping annotations job. Has status of '$annotations_status'"
         exit 1
     fi
 
